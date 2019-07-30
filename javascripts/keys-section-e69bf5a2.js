0638d5c08caf2210824332ff2fc5b2b43898e6da
Sara Scaini <sa.scaini@gmail.com>
2019-07-30 07:31:00 +0100

    Automated commit at 2019-07-30 06:30:59 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/keys-section-e69bf5a2.js
@@ -0,0 +1,19 @@
+
+const keys = document.querySelectorAll('.key');
+
+// const addTextOnMove = () => {
+//   key.addEventListener("mouseover", () => {
+
+//   const textKey = document.querySelector('#text-key');
+//   textKey.classList.remove("d-none");
+//   key.addEventListener("mouseout", () => {
+//     textKey.classList.add("d-none");
+//   })
+// });
+// }
+const keyTextex = document.querySelectorAll('.text-key');
+keyTextes.forEach(text => { text.addEventListener('')
+
+})
+
+export { addTextOnMove };
